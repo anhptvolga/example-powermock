74023d6db0c3c8c0720a8277800bcaa769ee8311
kpanic <anhptvolga@yandex.ru>
2019-08-03 12:09:40 +0700

    setup: add powermock dependency

---
+++ src/main/java/com/kpanic/example/Main.java
@@ -0,0 +1,12 @@
+package com.kpanic.example;
+
+/**
+ * Created by anh.phan3.
+ * Date: 2019-08-02
+ * Time: 16:40
+ */
+public class Main {
+    public static void main(String[] argv) {
+
+    }
+}
